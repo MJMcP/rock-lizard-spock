8aa21d859e841f22852cf627e49ee293683e9a83
MJMcP <maggiejury@gmail.com>
2022-08-07 19:17:23 +0000

    Reorganises functions for ease of reading

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ let computerScoreTotal = document.getElementById("comp-total");
 let resultText = document.querySelector(".declare");
 let resultsTally= document.querySelector(".tally > span > span");
 
-let play = document.querySelector(".picked");
+let plays = document.querySelector(".picked");
 const exitBtn = document.getElementById('btn-leave');
 
 const rockIcon = document.getElementById("rk");
@@ -16,64 +16,58 @@ const lizaIcon = document.getElementById("ld");
 const spocIcon = document.getElementById("sk");
 
 
-
 /**
-* 3rd created with assistance of freeCodeCamp youtube tutorial- creates the random 
-* generator for the comp choice
+* 1st created with the assistance of freeCodeCamp youtube tutorial - adds 
+* listener event to icons and console logs.
 */
-function gameCompChoice() {
-  let cChoices = ["rk", "pr", "ss", "ld", "sk"];
-  let cPick = (Math.floor(Math.random()* 5));
-  return cChoices[cPick];
-  console.log(gameCompChoice());
-}
+function main() {
+  rockIcon.addEventListener("click", function() { 
+    choice("rk");
+    console.log("rock");
+  })
 
+  papeIcon.addEventListener("click", function( ){
+    choice("pr");
+    console.log("paper");
+  })
 
+  scisIcon.addEventListener("click", function(){
+    choice("ss");
+    console.log("scissors");
+  })
 
-/**
-* 5th created with assistance of freeCodeCamp youtube tutorial - inserts a human 
-* readable word into the results notice the win() function's scoreBoard.innerHTML. 
-* This element will be displayed on the second play stage but for the moment, 
-* it is included here to ensure the coding actually works.
-*/
-function convertToWord(string) {
-  if (string === "rk") return "Rock";
-  if (string === "pr") return "Paper";
-  if (string === "ss") return "Scissors";
-  if (string === "ld") return "Lizard";
-  return "Spock";
-}
-  
-  
-/**
-* 4th created with assistance of freeCodeCamp youtube tutorial- creates the block 
-* code for the win scenario  
-*/
-function win(captChoice, computerChoice) { 
-  userScore++;
-  userScoreTotal.innerHTML = userScore;      
-  computerScoreTotal.innerHTML = computerScore;
-  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. That beats the computer's ${convertToWord(computerChoice)}. Proceed to next round!`;
-}
-  
-/**  6th created with assistance of freeCodeCamp youtube tutorial - creates the block 
-* code for the loss scenario
-*/ 
-function loss(captChoice, computerChoice) {
-  computerScore++;
-  userScoreTotal.innerHTML = userScore;   
-  computerScoreTotal.innerHTML = computerScore;
-  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. The computer chose ${convertToWord(computerChoice)}. Computer blocks your progress!`;
-}
+  lizaIcon.addEventListener("click", function(){
+    choice("ld");
+    console.log("lizard");
+  })
+
+  spocIcon.addEventListener("click", function(){
+    choice("sk");
+    console.log("spock")
     
-/**  7th created with the assistance of freeCodeCamp youtube tutorial - creates the block 
-* code for the draw scenario 
-*/
-function draw(captChoice, computerChoice) {
-  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. The computer chose ${convertToWord(computerChoice)}. Stalemate - you have another go!`;
-}
+  })
+ }
 
-  
+main();
+
+/**
+* creates the reload function on the exit button
+* inspired from Veronica Louren example.
+*/
+
+exitBtn.addEventListener('click', (_e) => {
+
+userScoreTotal.innerText = 0;
+computerScoreTotal.innerText = 0;
+resultText.innerHtml = "";
+
+// ensures settings returned to start settings.
+
+window.location.reload();
+
+});
+
+
 /**
 * 2nd created with the assistance of freeCodeCamp youtube tutorial - Begins by logging 
 * the selection to the console. . 
@@ -122,55 +116,63 @@ function choice(captChoice) {
   chosen.addEventListener("click", runGame());
 }
 
+
 /**
-* 1st created with the assistance of freeCodeCamp youtube tutorial - adds 
-* listener event to icons and console logs.
+* 3rd created with assistance of freeCodeCamp youtube tutorial- creates the random 
+* generator for the comp choice
 */
-function main() {
-    rockIcon.addEventListener("click", function() { 
-      choice("rk");
-      console.log("rock");
-    })
-  
-    papeIcon.addEventListener("click", function( ){
-      choice("pr");
-      console.log("paper");
-    })
-  
-    scisIcon.addEventListener("click", function(){
-      choice("ss");
-      console.log("scissors");
-    })
-  
-    lizaIcon.addEventListener("click", function(){
-      choice("ld");
-      console.log("lizard");
-    })
-  
-    spocIcon.addEventListener("click", function(){
-      choice("sk");
-      console.log("spock")
-      
-    })
-   }
-  
-main();
-  
+function gameCompChoice() {
+  let cChoices = ["rk", "pr", "ss", "ld", "sk"];
+  let cPick = (Math.floor(Math.random()* 5));
+  return cChoices[cPick];
+}
 
 
 /**
- * creates the reload function on the exit button
- * inspired from Veronica Louren example.
+* 5th created with assistance of freeCodeCamp youtube tutorial - inserts a human 
+* readable word into the results notice the win() function's scoreBoard.innerHTML. 
+* This element will be displayed on the second play stage but for the moment, 
+* it is included here to ensure the coding actually works.
 */
+function convertToWord(string) {
+  if (string === "rk") return "Rock";
+  if (string === "pr") return "Paper";
+  if (string === "ss") return "Scissors";
+  if (string === "ld") return "Lizard";
+  return "Spock";
+}
+  
+  
+/**
+* 4th created with assistance of freeCodeCamp youtube tutorial- creates the block 
+* code for the win loss and draw scenario  
+*/
+function win(captChoice, computerChoice) { 
+  userScore++;
+  userScoreTotal.innerHTML = userScore;      
+  computerScoreTotal.innerHTML = computerScore;
+  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. That beats the computer's ${convertToWord(computerChoice)}. Proceed to next round!`;
+}
 
-exitBtn.addEventListener('click', (e) => {
+function loss(captChoice, computerChoice) {
+  computerScore++;
+  userScoreTotal.innerHTML = userScore;   
+  computerScoreTotal.innerHTML = computerScore;
+  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. The computer chose ${convertToWord(computerChoice)}. Computer blocks your progress!`;
+}
+  
+function draw(captChoice, computerChoice) {
+  resultText.innerHTML = `You chose ${convertToWord(captChoice)}. The computer chose ${convertToWord(computerChoice)}. Stalemate - you have another go!`;
+}
 
-  userScoreTotal.innerText = 0;
-  computerScoreTotal.innerText = 0;
-  resultText.innerHtml = "";
+/**
+ * Creates the text for the end of game statement 
+ */
+function endGame(resultsTally){
+  if(userScore > computerScore ) {
+    resultText.innerHTML = `Congratulations! You have regained control of the ship. Time to Boldly Go...`;
+  } else if (computerScore > userScore) {
+    resultText.innerHTML = `RED ALERT! Computer has set the self destruct! Detonation in 5...4...3`;
+  }
+  }
 
-  // ensures settings returned to start settings.
-
-  window.location.reload();
-
-});
